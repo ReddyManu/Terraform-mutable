ae706d784ca106e10c72b9bb46a8642b9019ef77
Manu Reddy <reddybmanohar@gmail.com>
2022-01-08 22:32:13 +0530

    db data rds 6

--- db/data.tf
+++ db/data.tf
@@ -11,6 +11,11 @@ data "aws_secretsmanager_secret" "secrets" {
   name = var.ENV
 }
 
+output "sec" {
+  value = data.aws_secretsmanager_secret.secrets
+}
+
+
 #data "aws_secretsmanager_secret_version" "secrets-version" {
 #  secret_id = data.aws_secretsmanager_secret.secrets
 #}
