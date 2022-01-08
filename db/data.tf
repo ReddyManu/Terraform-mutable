146a27fd23affcddea60f06eed32585b9a0cb1a7
Manu Reddy <reddybmanohar@gmail.com>
2022-01-08 21:59:41 +0530

    db data rds

--- db/data.tf
+++ db/data.tf
@@ -6,3 +6,16 @@ data "terraform_remote_state" "vpc" {
     region = "us-east-1"
   }
 }
+
+data "aws_secretsmanager_secret" "secrets" {
+  name = var.ENV
+}
+
+data "aws_secretsmanager_secret_version" "secrets-version" {
+  secret_id = data.aws_secretsmanager_secret.secrets
+}
+
+resource "local_file" "foo" {
+  content  = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["RDS_MYSQL_USER"]
+  filename = "/tmp/1"
+}
