836962d9a8f6c2e5a49d7757bec663087cbf5778
Manu Reddy <reddybmanohar@gmail.com>
2022-01-09 03:43:46 +0530

    db data 9

--- db/mysql-rds.tf
+++ db/mysql-rds.tf
@@ -1,16 +1,16 @@
-#resource "aws_db_instance" "mysql" {
-#  allocated_storage    = 10
-#  identifier           = "mysql-${var.ENV}"
-#  engine               = "mysql"
-#  engine_version       = "5.7"
-#  instance_class       = "db.t2.micro"
-#  name                 = "dummy"
-#  username             = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["RDS_MYSQL_USER"]
-#  password             = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["RDS_MYSQL_PASS"]
-#  parameter_group_name = aws_db_parameter_group.pg.name
-#  skip_final_snapshot  = true
-#}
-#
+resource "aws_db_instance" "mysql" {
+  allocated_storage    = 10
+  identifier           = "mysql-${var.ENV}"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "dummy"
+  username             = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["RDS_MYSQL_USER"]
+  password             = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["RDS_MYSQL_PASS"]
+  parameter_group_name = aws_db_parameter_group.pg.name
+  skip_final_snapshot  = true
+}
+
 resource "aws_db_parameter_group" "pg" {
   name   = "mysql-${var.ENV}-pg"
   family = "mysql5.7"
