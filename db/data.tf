cd3c3aae0d961c2677ec1d5de6509ad3dd7a7218
Manu Reddy <reddybmanohar@gmail.com>
2022-01-09 03:41:17 +0530

    db data 8

--- db/data.tf
+++ db/data.tf
@@ -15,15 +15,10 @@ data "aws_secretsmanager_secret_version" "secrets-version" {
   secret_id = data.aws_secretsmanager_secret.secrets.id
 }
 
-resource "null_resource" "test" {
-  provisioner "local-exec" {
-    command = "echo ${jsondecode(data.aws_secretsmanager_secret_version.secrets-version)["RDS_MYSQL_USER"]} >/tmp/1"
-  }
+resource "local_file" "foo" {
+  content  = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["RDS_MYSQL_USER"]
+  filename = "/tmp/1"
 }
-#resource "local_file" "foo" {
-#  content  = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["RDS_MYSQL_USER"]
-#  filename = "/tmp/1"
-#}
 
 #output "sec" {
 #  value = data.aws_secretsmanager_secret_version.secrets-version
