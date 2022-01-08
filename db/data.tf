d4b4804f4e827fc820bfeb950aaaed33e5737c22
Manu Reddy <reddybmanohar@gmail.com>
2022-01-08 19:35:52 +0530

    db data

--- db/data.tf
+++ db/data.tf
@@ -1,6 +1,6 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
-  config {
+  config = {
     bucket = "b60-terraform"
     key    = "terraform-mutable/vpc/${var.ENV}/terraform.tfstate"
     region = "us-east-1"
