e69a53d4c0fc11b84a59c9f18cca07d6e01bae28
Manu Reddy <reddybmanohar@gmail.com>
2022-01-10 01:57:09 +0530

    alb 1

---
+++ alb/data.tf
@@ -0,0 +1,21 @@
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "b60-terraform"
+    key    = "terraform-mutable/vpc/${var.ENV}/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "aws_secretsmanager_secret" "secrets" {
+  name = var.ENV
+}
+
+data "aws_secretsmanager_secret_version" "secrets-version" {
+  secret_id = data.aws_secretsmanager_secret.secrets.id
+}
+
+
+
+
+
---
+++ alb/env/dev-backend.tfvars
@@ -0,0 +1,4 @@
+bucket = "b60-terraform"
+key    = "terraform-mutable/alb/dev/terraform.tfstate"
+region = "us-east-1"
+
