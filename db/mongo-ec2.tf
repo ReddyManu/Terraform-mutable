8d7596c7b60baf832a920d2846792f62e181988d
Manu Reddy <reddybmanohar@gmail.com>
2022-01-10 00:03:29 +0530

    db mongo 5

--- db/mongo-ec2.tf
+++ db/mongo-ec2.tf
@@ -79,9 +79,9 @@ resource "null_resource" "mongodb-setup" {
       password = local.ssh_pass
     }
     inline = [
-      "yum install python3-pip -y",
-      "pip3 install pip --upgrade",
-      "pip3 install ansible",
+      "sudo yum install python3-pip -y",
+      "sudo pip3 install pip --upgrade",
+      "sudo pip3 install ansible",
       "ansible-pull -U https://github.com/ReddyManu/Ansible.git roboshop-pull.yml -e ENV=${var.ENV}"
     ]
   }
