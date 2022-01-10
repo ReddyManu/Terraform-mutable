c410db011a47034c3fb32f3c9740a0d6441caf18
Manu Reddy <reddybmanohar@gmail.com>
2022-01-11 03:36:06 +0530

    T-m app-mod ec2

--- app-module/ec2.tf
+++ app-module/ec2.tf
@@ -4,6 +4,9 @@ resource "aws_instance" "od" {
   instance_type = var.INSTANCE_TYPE
   subnet_id = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS, count.index)
   vpc_security_group_ids = [aws_security_group.sg.id]
+  tags = {
+    Name = local.tags["Name"]
+  }
 }
 
 resource "aws_spot_instance_request" "spot" {
