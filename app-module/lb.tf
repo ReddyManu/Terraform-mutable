d8ed82f5716bb895f17f06e2f94570dbd2fa71f0
Manu Reddy <reddybmanohar@gmail.com>
2022-01-11 02:33:19 +0530

    frontend t-m main & T-m app-mod lb

--- app-module/lb.tf
+++ app-module/lb.tf
@@ -20,7 +20,8 @@ resource "aws_lb_target_group_attachment" "tg-attach" {
   port             = var.PORT
 }
 
-resource "aws_lb_listener_rule" "static" {
+resource "aws_lb_listener_rule" "private" {
+  count        = var.IS_PRIVATE_LB ? 1 : 0
   listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
   priority     = var.LB_RULE_PRIORITY
 
@@ -33,5 +34,34 @@ resource "aws_lb_listener_rule" "static" {
     host_header {
       values = ["${var.COMPONENT}-${var.ENV}-roboshop.internal"]
     }
+  }
+}
+
+resource "aws_lb_listener_rule" "public" {
+  count        = var.IS_PRIVATE_LB ? 1 : 0
+  listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
+  priority     = var.LB_RULE_PRIORITY
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.COMPONENT}-${var.ENV}-roboshop.internal"]
+    }
+  }
+}
+
+resource "aws_lb_listener" "private-listener" {
+  count             = var.IS_PRIVATE_LB ? 0 : 1
+  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ARN
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
   }
 }
